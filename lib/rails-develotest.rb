9d6811c76fdb643de4891f8b9bf2547c73e29ea5
Niklas Hofer <niklas+dev@lanpartei.de>
2013-04-25 17:43:48 +0200

    extract from Rails app

--- lib/rails-develotest.rb
+++ lib/rails-develotest.rb
@@ -1,7 +1,21 @@
 require "rails-develotest/version"
 
-module Rails
-  module Develotest
-    # Your code goes here...
+module Develotest
+  Positive = %w(true yes y 1 yeah jawoll si)
+  def self.enabled?
+    environment_variable_enabled?
+  end
+
+  def self.environment_variable_enabled?
+    Positive.include? ENV['DEVELOTEST'].to_s
+  end
+
+  # run this from your App::Application.configure blog in config/environments/*
+  def self.setup(config)
+    if enabled?
+      config.cache_classes = false
+      config.assets.compress = false
+      config.assets.debug = true
+    end
   end
 end
